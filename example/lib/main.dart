e035256a01416215d2b28b2a8b52c28254d52b54
Roni <papai983g@gmail.com>
2020-08-10 14:21:17 +0530

    Add features

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
               }),
             ),
             itemCount: _imageUris.length,
-            onWillAccept: (data, pos) => true,
+            onWillAccept: (oldIndex, newIndex) => true,
             onReorder: (oldIndex, newIndex) {
               final temp = _imageUris[oldIndex];
               _imageUris[oldIndex] = _imageUris[newIndex];
